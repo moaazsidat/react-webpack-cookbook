095fdfbfa89923e4a841c8474738a88df9e9285f
Moaaz Sidat <moaaz.omer@gmail.com>
2015-09-06 21:10:20 -0700

    lazy loaded entry points

--- app/main.js
+++ app/main.js
@@ -1,11 +1,32 @@
 'use strict'
 import React from 'react';
 import Hello from './modules/Component.jsx';
+import Feed from './Feed.js'
 import './main.css';
 
 
-main();
 
-function main() {
-	React.render (<Hello />, document.getElementById('app'));
+class App extends React.Component {
+	constructor() {
+		this.state = { currentComponent: Feed };
+	}
+	openProfile() {
+		/*
+		What is the array on the first argument?
+		If you try to lazy load a chunk that 
+		depends on an other lazy loaded chunk you 
+		can set it as a dependency in the array. 
+		*/
+		require.ensure(['./FunnyButton.js'], () => {
+			var Profile = require('.Profile.js');
+			this.setState({
+				currentComponent: Profile;
+			});
+		});
+	}
+	render() {
+		return (
+			return <div>{this.state.currentComponent()}</div>
+		);
+	}
 }
